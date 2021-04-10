496b60ae8c06abe1c75a6d7fa50bb92640958f53
Matthew Rodatus <matthew@rodatus.org>
2021-04-10 08:48:24 -0400

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace windows_service_wrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
